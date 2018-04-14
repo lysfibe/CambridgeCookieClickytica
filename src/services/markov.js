c7f4449a0675dafec9d172e1fa322a056a84132c
David Ralph <David.M.L.Ralph@gmail.com>
2018-04-14 13:08:14 +0100

    Improved Markov code quality
    - Added convenience function markov.string(options) to return just the generated string.
    
    - Added documentation to Markov code.
    
    - Refactored various code for Markov

--- src/services/markov.js
+++ src/services/markov.js
@@ -1,14 +1,14 @@
+/**
+ * Wrapper for Markov string generator.
+ * See markovTest.js for example usage.
+ */
 const fs = require("fs");
 const Markov = require('markov-strings');
 
 const defaultCorpusPath = 'src/services/defaultCorpus.txt';
 
 // Load default data
-const defaultData = fs
-    .readFileSync(defaultCorpusPath)
-    .toString()
-    .split('\n')
-    .filter(Boolean);
+const defaultData = fs.readFileSync(defaultCorpusPath).toString();
 
 // Some options to generate Twitter-ready strings
 const defaultOptions = {
@@ -23,22 +23,43 @@ async function init(data, options) {
         data = data || defaultData;
         options = options || defaultOptions;
 
-        const existingFilter = options.filter;
+        data = clean(data);
 
-        options.filter = result => {
-            // Existing filter
-            if (existingFilter && !existingFilter(result)) return false;
-            // Not quoting corpus
-            return data.indexOf(result.string) === -1;
-        }
+        options = setFilter(options, data);
 
+        // Construct (potentially slow)
         const markov = new Markov(data, options);
         await markov.buildCorpus();
+
+        // Convenience method
         markov.string = opt => markov.generateSentence(opt).then(r => r.string);
+        
         return markov;
     } catch (err) {
         console.error('Markov died:\n', err);
     }
+}
+
+function clean(data) {
+    // For arrays of strings containing \n
+    if (Array.isArray(data)) data = data.join('\n');
+
+    // Convert to string if unexpected input
+    if (typeof data !== 'string') data = data.toString();
+
+    // Convert to array of strings, remove empty strings
+    return data.split('\n').filter(Boolean);
+}
+
+function setFilter(options, data) {
+    const existingFilter = options.filter;
+    options.filter = result => {
+        // Existing filter
+        if (existingFilter && !existingFilter(result)) return false;
+        // Not quoting corpus
+        return data.indexOf(result.string) === -1;
+    }
+    return options;
 }
 
 console.log('Initializing Markov');
--- src/services/markovTest.js
+++ src/services/markovTest.js
@@ -1,11 +1,13 @@
+/**
+ * Run (or require) this file to output some test Markov strings to the console.
+ */
 const markov = require('./markov');
-
-async function test() {
-    const mark = await markov.init();
-
-    console.log('TEST MARKOV DEFAULT:\n', await mark.string());
-
-    console.log('TEST MARKOV 50 CHAR:\n', await mark.string({ maxLength: 50 }));
-}
-
 test();
+async function test() {
+    const m = await markov.init();
+
+    console.log('TEST MARKOV DEFAULT:\n', await m.string());
+
+    console.log('TEST MARKOV 50 CHAR:\n', await m.string({ maxLength: 50 }));
+
+}
