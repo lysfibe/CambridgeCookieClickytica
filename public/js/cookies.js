9e1a4ef647a8bae65b780d4de050e400eb60acf3
Louis Capitanchik <contact@louiscap.co>
2018-04-14 15:36:23 +0100

    Handle buyables

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -3,6 +3,7 @@ $(function cookies() {
 	const els = {
 		posts: $('#post-count'),
 		linkedAccounts: $('#linked-accounts-count'),
+		buyableLists: $(''),
 	}
 
 	const ui = {
@@ -11,7 +12,10 @@ $(function cookies() {
 		},
 		updateLinked(n) {
 			els.linkedAccounts.text(n)
-		}
+		},
+		updateBuyable(list) {
+			
+		},
 	}
 
 	const state = (function loadState() {
@@ -32,6 +36,24 @@ $(function cookies() {
 	ui.updatePosts(state.posts)
 	ui.updateLinked(state.linkedAccounts)
 
+	let locked = [
+		{
+			name: '',
+			content: 'Increase your PPC and get an extra Post Per Click',
+			price: 10,
+			purchased: 0,
+			rate: 10,
+		},
+		{
+			name: '',
+			content: 'Link your friends account',
+			price: 50,
+			purchased: 0,
+			rate: 50,
+		}
+	]
+	let buyable = []
+	let bought = []
 
 	let linkedAccountTimer = 0
 	let lastTick = performance.now()
@@ -39,25 +61,53 @@ $(function cookies() {
 	const stateListeners = []
 
 	function getStateModifiers() {
-		return 1 + state.postBonus
+		return 1 + bought.map(p => p.perks.postBonus).reduce((t, c) => t + c, 0)
 	}
-
 
 	function addPost(n = 1) {
 		let oldState = Object.assign({}, state)
 		state.posts += n
 		stateListeners.map(l => l(oldState, state))
+		triggerRealThumb(n)
 	}
 	
 	function addLinkedAccount(n = 1) {
 		if (state.linkedAccounts < 1 && state.linkedAccounts + n > 0) {
-			console.log("foo")
 			linkedAccountTimer = 0
 			setTimeout(() => tickLinkedAccount(performance.now()))
 		}
 		let oldState = Object.assign({}, state)
 		state.linkedAccounts += n
 		stateListeners.map(l => l(oldState, state))
+	}
+
+	function triggerRealThumb(thumbs) {
+		return
+		for (let i = 0; i < thumbs; i += 1) {
+			const image = new Image(300, 400)
+			image.src = '/images/real_thumb.png'
+			image.style.position = 'absolute'
+			image.style.top = String(0)
+			console.log(window.innerWidth)
+			image.left = String(window.innerWidth)
+			console.log(image.style)
+			let lastTime = Date.now()
+			let speed = 10
+			function dothumbo() {
+				const now = Date.now()
+				const delta = now - lastTime
+				// console.log(delta)
+				image.style.left = parseInt(image.style.left, 10) - (speed * delta)
+				if (image.style.left + image.width > 10) {
+					setTimeout(dothumbo)
+				} else {
+					document.body.removeChild(image)
+				}
+			}
+			
+			document.body.appendChild(image)
+			setTimeout(dothumbo)
+		}
 	}
 
 	$(document.body).on('click', '#add-post', () => {
@@ -85,6 +135,19 @@ $(function cookies() {
 			ui.updateLinked(current.linkedAccounts)
 		}
 	})
+
+	stateListeners.push((_, state) => {
+		const getPrice = p => p.price + (p.purchased * p.rate)
+		
+		const canNowBuy = locked.filter(p => getPrice(p) <= state.posts)
+		const cantBuy = buyable.filter(p => getPrice(p) > state.posts)
+
+		locked = locked.filter(p => getPrice(p) > state.posts).concat(cantBuy)
+		buyable = buyable.filter(p => getPrice(p) <= state.posts).concat(canNowBuy)
+
+		ui.updateBuyable(buyable)
+	})
+
 
 	window._debug = {
 		addLinkedAccount,
