bf4dead704d8ac485da386d97d1cea275e72395d
David Ralph <David.M.L.Ralph@gmail.com>
2018-04-14 16:35:21 +0100

    Datasets

--- src/services/markovTest.js
+++ src/services/markovTest.js
@@ -11,19 +11,19 @@ async function test() {
     console.log('TEST MARKOV DEFAULT:\n', await dm.string());
 
     // Test new model ----------------------------------------------------------
-    const m = await markov.init({ path: 'data/input.txt' }, { stateSize: 1 });
+    const m = await markov.init({ path: 'data/tweets.txt' }, { stateSize: 1 });
 
     console.log('TEST MARKOV NO-OPT:\n', await m.string());
 
     console.log('TEST MARKOV 500 CHAR:\n', await m.string({ maxLength: 500 }));
 
-    // Test performance
-    let start = Date.now();
-    const tasks = [];
-    for (let i = 0; i < 100; i++) {
-        tasks.push(m.string());
-    }
-    await Promise.all(tasks);
-    const diff = Date.now() - start;
-    console.log('COMPLETED 100 MARKOVS IN ' + diff / 1000 + ' SECONDS');
+    // // Test performance
+    // let start = Date.now();
+    // const tasks = [];
+    // for (let i = 0; i < 100; i++) {
+    //     tasks.push(m.string());
+    // }
+    // await Promise.all(tasks);
+    // const diff = Date.now() - start;
+    // console.log('COMPLETED 100 MARKOVS IN ' + diff / 1000 + ' SECONDS');
 }
