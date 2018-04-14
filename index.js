4bcdb011d59621a6174ab4e7d3f67cb22a8a87f8
Louis Capitanchik <contact@louiscap.co>
2018-04-14 11:55:49 +0100

    Basic routing, include socket.io

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const pathUtils = require('path')
 const server = new (require('@commander-lol/server'))
 const mustache = require('@commander-lol/koa-mustache')
+const Socket = require('koa-socket')
 
 server.serve('/', pathUtils.join(__dirname, 'public'))
 server.use(mustache(pathUtils.join(__dirname, 'views')))
 server.routes(require('./src/http/routes'))
 
+const socket = new Socket()
+
+socket.use(ctx => {
+	console.log(ctx)
+})
+
+socket.attach(server)
 server.listen(8000)
 console.log("Listening on http://localhost:8000")
--- src/http/routes.js
+++ src/http/routes.js
@@ -1,3 +1,4 @@
 module.exports = function(router) {
 	router.get('/', ctx => ctx.render('index', { title: 'CambridgeCookieClickytica' }))
+	router.group('/api', require('./routes-api'))
 }
