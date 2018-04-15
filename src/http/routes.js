c37e089624716b8483d5dafc389b7793970cee8b
David Ralph <David.M.L.Ralph@gmail.com>
2018-04-15 18:10:08 +0100

    Faster Markov

--- src/http/routes.js
+++ src/http/routes.js
@@ -1,6 +1,9 @@
 const random_name = require('node-random-name');
 const markov = require('../services/markov');
 
+//let m = markov.default;
+let m = markov.init({ path: 'data/tweets.txt' }, { stateSize: 1 }).then(console.log('tt'));
+
 module.exports = function (router) {
 	router.get('/', ctx => ctx.render('index', { title: 'CambridgeCookieClickytica' }))
 	router.get('/posts', async ctx => {
@@ -10,13 +13,17 @@ module.exports = function (router) {
 }
 
 async function getPosts(number = 10) {
-	//const m = await markov.default;
-	const m = await markov.init({ path: 'data/tweets.txt' }, { stateSize: 1 });
+	m = await m; // Ensure Markov is ready
+
+	// Generate posts
 	const tasks = [];
 	for (let i = 0; i < number; i++) {
 		tasks.push(m.string());
 	}
 	let posts = await Promise.all(tasks);
+	
+	// Add usernames
 	posts = posts.map(content => ({ content, username: random_name() }));
+	
 	return posts;
 }
